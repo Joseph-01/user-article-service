7174f58537f153973f0adffc947de0f019b99901
Joseph-01 <softjoe40@gmail.com>
2022-11-03 10:52:44 +0100

    create docs for article/post model

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,75 @@ const { findById } = require("../models/post")
 const Post = require("../models/post")
 const User = require("../models/user")
 
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - body
+ *         - title
+ *         - userId
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the post
+ *         userId:
+ *           type: string
+ *         title:
+ *           type: stringe
+ *         image:
+ *           type: string
+ *         body:
+ *           type: string
+ *         likes:
+ *           type: array
+ *         categories:
+ *           type: array
+ *       example:
+ *         id: dkdcnvfe8e89
+ *         userId: 63586f767c5dd57d7e0d3a1c
+ *         title: The New Turing Omnibus
+ *         caption: caption for post three
+ *         body: Lorem Ipsum Lorem Ipsum dolor sit amet, consectetuer adipiscingelit. Duis tellus. Donec ante dolor, iaculis nec, gravidaac, cursus in, eros. Mauris vestibulum, felis et egestasullamcorper, purus nibh vehicula sem, eu egestas antenisl non justo. Fusce tincidunt, lorem nev dapibusconsectetuer, leo orci mollis ipsum, eget suscipit erospurus in ante. At ipsum vitae est lacinia tincidunt. Maecenas elit orci,gravida ut, molestie non, venenatis vel, lorem. Sedlacinia. Suspendisse potenti. Sed ultricies cursuslectus. In id magna sit amet nibh suspicit euismod.Integer enim. Donec sapien ante, accumsan ut,sodales commodo, auctor quis, lacus. Maecenas a elitlacinia urna posuere sodales. Curabitur pede pede,molestie id, blandit vitae, varius ac, purus. Mauris atipsum vitae est lacinia tincidunt. Maecenas elit orci, gravida ut, molestie non, venenatis vel,lorem. Sed lacinia. Suspendisse potenti. Sed ultrucies cursus lectus. In id magna sit amet nibhsuspicit euismod. Integer enim. Donec sapien ante, accumsan ut, sodales commodo, auctorquis, lacus. Maecenas a elit lacinia urna posuere sodales. Curabitur pede pede, molestie id,blandit vitae, varius ac, purus. Morbi dictum. Vestibulum adipiscing pulvinar quam. In aliquam rhoncus sem. In mi erat, sodaleseget, pretium interdum, malesuada ac, augue. Aliquam sollicitudin, massa ut vestibulum posuere, massa arcu elementumpurus, eget vehicula lorem metus vel libero. Sed in dui id lectus commodo elementum. Etiam rhoncus tortor. Proin alorem. Ut nec velit. Quisque varius. Proin nonummy justo dictum sapien tincidunt iaculis. Duis lobortis pellentesque risus.Aenean ut tortor imperdiet dolor scelerisque bibendum. Fusce metus nibh, adipiscing id, ullamcorper at, consequat a,nulla
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Posts
+ *  description: The posts managing API
+*/
+
+/**
+ * @swagger
+ * /posts:
+ *  get:
+ *   summary: Returns the list of all the posts
+ *   tags: [Posts]
+ *   responses:
+ *      200:
+ *        description: The list of the posts
+ *        content:
+ *          application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/Post'
+ *      500:
+ *         description: Some server error
+ */
 const getAllPost = async (req, res) => {
     try {
         const post = await Post.find({})
@@ -11,7 +80,30 @@ const getAllPost = async (req, res) => {
     }
 }
 
-//create a post
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: The post was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       500:
+ *         description: Some server error
+ */
 const createPost = async (req, res) => {
     try {
         const newPost = new Post(req.body)
@@ -23,7 +115,31 @@ const createPost = async (req, res) => {
 }
 
 
-//get a post by id
+/**
+ * @swagger
+ * /posts/{id}:
+ *  get:
+ *   summary: Get the post by id
+ *   tags: [Posts]
+ *   parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: string
+ *       required: true
+ *       description: The post id
+ *   responses:
+ *    200:
+ *      description: The post description by id
+ *      contents:
+ *        application/json:
+ *          schema:
+ *            type: array 
+ *            items:
+ *              $ref: '#/components/schemas/Post'
+ *    404:
+ *      description: The post was not found
+ */
 const getPostById = async (req, res) => {
     try {
         const postId = req.params.id
@@ -37,7 +153,39 @@ const getPostById = async (req, res) => {
     }
 }
 
-//update a post
+/**
+ * @swagger
+ * /posts/{id}:
+ *  put:
+ *   summary: Update the post by id
+ *   tags: [Posts]
+ *   security:
+ *     - bearerAuth: []
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/Post'
+ *   parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: string
+ *       required: true
+ *       description: The post id
+ *   responses:
+ *    200:
+ *      description: The post description by id
+ *      contents:
+ *        application/json:
+ *          schema:
+ *            type: array 
+ *            items:
+ *              $ref: '#/components/schemas/Post'
+ *    404:
+ *      description: The post was not found
+ */
 const updatePost = async (req, res) => {
     try {
         const id = req.params.id
@@ -60,7 +208,7 @@ const updatePost = async (req, res) => {
 const deletePost = async (req, res) => {
     try {
         const postToCheck = await Post.findById(req.params.id)
-        if(!postToCheck) {
+        if (!postToCheck) {
             return res.status(404).json({ "errMsg": "post not found" })
         }
         await Post.findByIdAndDelete(req.params.id)
--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const { urlencoded } = require("express");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config()
 const port = process.env.PORT || 3000
+const swaggerUI = require("swagger-ui-express")
+const swaggerDocs = require("swagger-jsdoc")
 //config file
 const dbConnection = require("./config/config");
+
+//swagger options
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "user-article-service",
+            version: "0.0.1",
+            description: "This is the service for creating, updating, deleting and getting users details, and also for anything relating to the article service"
+        },
+        servers: [
+            {
+                //url: "http://localhost:3000"
+                url: "https://article-api-shugr.cyclic.app"
+            }
+        ]
+    },
+    apis: [
+        "./controllers/*.js"
+    ]
+}
+
+const specs = swaggerDocs(options)
+
 //routes
 const userRoute = require("./routes/userRoutes")
 const postRoute = require("./routes/postRoutes")
 
 
 // app.use(urlencoded({extended: true}))
+app.use(cors())
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 app.use(express.json())
 app.use("/users", userRoute)
 app.use("/posts", postRoute)
